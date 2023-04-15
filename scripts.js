f8e4dccff357042fd268c1be9811df1050325b93
RicardoFT1 <ricardo.fandinot@ecci.edu.co>
2023-04-15 13:25:52 -0500

    terminado

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
-function changeButtonColor() {
-  document.querySelector("button").style.backgroundColor = "blue";
-}
 document.querySelector("button").addEventListener("click", function() {
-  window.location.href = "https://api.whatsapp.com/send?phone=573134570104&text=¡Quiero%20unirme%20a%20la%20fiesta%20de%20la%20revelación%20de%20sexo!%20¿Será%20un%20niño%20o%20una%20niña?%20No%20puedo%20esperar%20para%20descubrirlo%20y%20celebrar%20junto%20a%20todos.%20¡Vamos%20a%20hacerlo%20un%20día%20inolvidable!";
+  window.location.href = "https://api.whatsapp.com/send?phone=573195605371&text=¡Quiero%20unirme%20a%20la%20fiesta%20de%20la%20revelación%20de%20sexo!%20¿Será%20un%20niño%20o%20una%20niña?%20No%20puedo%20esperar%20para%20descubrirlo%20y%20celebrar%20junto%20a%20todos.%20¡Vamos%20a%20hacerlo%20un%20día%20inolvidable!";
 }); 
 window.addEventListener('load', () => {
   const canvas = document.createElement('canvas');
